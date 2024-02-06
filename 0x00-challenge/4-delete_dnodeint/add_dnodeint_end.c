997a8f8c80c4a5247f443e3fd6c1437f88476764
mohanned-sameh <mohanned2678@gmail.com>
2024-02-06 03:06:39 +0200

    Refactor add_dnodeint_end and delete_dnodeint_at_index functions

--- 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ 0x00-challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -12,7 +12,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *last;
+	dlistint_t *new, *current;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -23,12 +23,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new->prev = NULL;
 		*head = new;
-		return (new);
 	}
-	last = *head;
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new;
-	new->prev = last;
+	else
+	{
+		current = *head;
+		while (current->next)
+			current = current->next;
+		current->next = new;
+		new->prev = current;
+	}
 	return (new);
 }
